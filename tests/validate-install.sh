cde8cf8e90b4fb53221419638fd82f1a0490de12
Naftuli Kay <rfkrocktk@gmail.com>
2017-01-08 14:55:33 -0800

    Working toward it.

--- tests/validate-install.sh
+++ tests/validate-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # not here to screw around
-set -ex
+set -e
 
 # grep the bashrc
 grep -iP '^export (GOROOT|GOPATH|PATH)=.*' ~/.bashrc
@@ -10,7 +10,10 @@ grep -iP '^export (GOROOT|GOPATH|PATH)=.*' ~/.bashrc
 source ~/.bashrc
 
 # find out which go we're talking about
-which go
+if [ "$(which go)" != "$HOME/go${GO_VERSION}/bin/go" ]; then
+  echo "Invilid Go Binary: $(which go)" >&2
+  exit 1
+fi
 
 # get go environment
 go env
