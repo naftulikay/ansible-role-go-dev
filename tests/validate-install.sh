9fb5ce6d9502e3fb0f21564a181130f016e9c142
Naftuli Kay <rfkrocktk@gmail.com>
2017-01-08 16:21:44 -0800

    Update tests.

--- tests/validate-install.sh
+++ tests/validate-install.sh
@@ -3,9 +3,6 @@
 # not here to screw around
 set -e
 
-# grep the bashrc
-grep -iP '^export (GOROOT|GOPATH|PATH)=.*' ~/.travis-bashrc
-
 # source it
 source ~/.travis-bashrc
 
@@ -13,18 +10,24 @@ source ~/.travis-bashrc
 if [ "$GOROOT" != "$HOME/go${GO_VERSION}" ]; then
   echo "Invalid GOROOT: $GOROOT" >&2
   exit 1
+else
+  echo "GOROOT is valid."
 fi
 
 # determine GOPATH
 if [ "$GOPATH" != "$HOME/gopath" ]; then
   echo "Invalid GOPATH: $GOPATH" >&2
   exit 1
+else
+  echo "GOPATH is valid."
 fi
 
 # find out which go we're talking about
 if [ "$(which go)" != "$HOME/go${GO_VERSION}/bin/go" ]; then
-  echo "Invilid Go Binary: $(which go)" >&2
+  echo "Invalid Go Binary: $(which go)" >&2
   exit 1
+else
+  echo "Valid Go Binary on PATH."
 fi
 
 # get go environment
